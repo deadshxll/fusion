5c478b337d1ef6b49b402669ba702025d0b3ae83
Lefraudeur <karlbouisseren@gmail.com>
2023-08-14 18:22:48 +0200

    Added asm source code and removed useless method

--- asm/src/main/java/io/github/lefraudeur/ClassPatcher.java
+++ asm/src/main/java/io/github/lefraudeur/ClassPatcher.java
@@ -35,34 +35,4 @@ public class ClassPatcher {
         classReader.accept(classVisitor, 0);
         return classWriter.toByteArray();
     }
-
-    public static byte[] patchClientBrandRetriever(byte[] classBytes, String getClientModName, String ThreadContext, String EMPTY_MAP) {
-        ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS);
-        ClassVisitor classVisitor = new ClassVisitor(Opcodes.ASM5, classWriter) {
-            @Override
-            public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
-                if (name.equals(getClientModName)) {
-                    return new MethodVisitor(Opcodes.ASM5, cv.visitMethod(access, name, descriptor, signature, exceptions)) {
-                        @Override
-                        public void visitInsn(int opcode) {
-                            if (opcode == Opcodes.ARETURN) {
-                                mv.visitInsn(Opcodes.POP);
-                                mv.visitFieldInsn(Opcodes.GETSTATIC, ThreadContext, EMPTY_MAP, "Ljava/util/Map;");
-                                mv.visitLdcInsn("client_brand");
-                                mv.visitMethodInsn(Opcodes.INVOKEINTERFACE, "java/util/Map", "get", "(Ljava/lang/Object;)Ljava/lang/Object;", true);
-                                mv.visitTypeInsn(Opcodes.CHECKCAST, "java/lang/String");
-                                mv.visitInsn(Opcodes.ARETURN);
-                                return;
-                            }
-                            mv.visitInsn(opcode);
-                        }
-                    };
-                }
-                return cv.visitMethod(access, name, descriptor, signature, exceptions);
-            }
-        };
-        ClassReader classReader = new ClassReader(classBytes);
-        classReader.accept(classVisitor, 0);
-        return classWriter.toByteArray();
-    }
 }
